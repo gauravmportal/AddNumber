2458760dac2e79df8626fb9e9c4f1a83b659362b
gaurav.mportal <gaurav.mportal@gmail.com>
2016-07-23 18:26:55 +0530

    Inital commit

---
+++ AddNumber/src/calculator/addnumber/addition.java
@@ -0,0 +1,15 @@
+package calculator.addnumber;
+
+public class addition {
+	
+	public static void main(String args [])
+	{
+		System.out.println("Adding two number");
+        System.out.println("this program written by gaurav");
+        int x,y,z;
+        x=10;y=90;
+        z=x+y;
+        System.out.println("addtion of 2 number: " +z);
+	}
+
+}
